c826e5a8f9941b262940938043181f0d6dc2740a
haotran55 <haot54592@gmail.com>
2025-04-09 17:42:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def ask():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Render sẽ set PORT này
+    app.run(host='0.0.0.0', port=port)
+
